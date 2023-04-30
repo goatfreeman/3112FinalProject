ce3e983f2736f8ceba522dddf8fd0deba80709fc
Matthew Ho <matthewho1999@gmail.com>
2023-04-30 19:48:19 -0400

    creating a file if noveldatabase.txt is not found

--- 3112FinalProject/landPage.cs
+++ 3112FinalProject/landPage.cs
@@ -8,6 +8,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.IO;
 
 namespace _3112FinalProject {
     public partial class landPage : Form {
@@ -20,6 +21,20 @@ namespace _3112FinalProject {
         private void Form3_Load(object sender, EventArgs e) {
             cn = new SqlConnection(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=|DataDirectory|\Database1.mdf;Integrated Security=True");
             cn.Open();
+            try {
+                if (File.Exists("noveldatabase.txt")) {
+
+                }
+                else {
+                    using (FileStream fs1 = File.Create("noveldatabase.txt"));
+                    MessageBox.Show("Database not found!\nCreating new Database", "Done", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+            }
+            catch(Exception Ex) {
+                Console.WriteLine(Ex.ToString());
+            }
+
+
         }
 
         private void label1_Click(object sender, EventArgs e) {
