5784b1e7309fb30e748de075469da8303c8f75c5
Matthew Ho <matthewho1999@gmail.com>
2023-04-30 21:42:14 -0400

    Ditching sql database as it stopped working ;D

--- 3112FinalProject/crUser.cs
+++ 3112FinalProject/crUser.cs
@@ -2,24 +2,24 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
-using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.IO;
+
 
 namespace _3112FinalProject {
     public partial class crUser : Form {
-        SqlCommand cmd;
-        SqlConnection cn;
-        SqlDataReader dr;
+
+        public object Filestream { get; private set; }
+
         public crUser() {
             InitializeComponent();
         }
         private void Form2_Load(object sender, EventArgs e) {
-            cn = new SqlConnection(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=|DataDirectory|\Database1.mdf;Integrated Security=True");
-            cn.Open();
+
         }
 
         private void label2_Click(object sender, EventArgs e) {
@@ -29,21 +29,7 @@ namespace _3112FinalProject {
         private void button2_Click(object sender, EventArgs e) {
             if (username.Text != string.Empty || password.Text != string.Empty || passwordConfirm.Text != string.Empty) {
                 if(password.Text == passwordConfirm.Text) {
-                    cmd = new SqlCommand("select * from LoginTable where username='" + username.Text + "'", cn);
-                    dr = cmd.ExecuteReader();
-                    if (dr.Read()) {
-                        dr.Close();
-                        MessageBox.Show("Username Taken\nTry Login","Error",MessageBoxButtons.OK,MessageBoxIcon.Error);
-                    }
-                    else {
-                        dr.Close();
-                        cmd = new SqlCommand("insert into LoginTable (Id,username,password) values(NEWID(),@username,@password)", cn);
-                        cmd.Parameters.AddWithValue("username", username.Text);
-                        cmd.Parameters.AddWithValue("password", password.Text);
-                        cmd.ExecuteNonQuery();
-                        MessageBox.Show("Account created.","Done",MessageBoxButtons.OK,MessageBoxIcon.Information);
-                        this.Close();
-                    }
+                    createUser(username.Text,password.Text);
                 }
                 else {
                     MessageBox.Show("Password and Password confirm mismatch\nPlease re-enter your password", "Error", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
@@ -56,6 +42,35 @@ namespace _3112FinalProject {
 
         private void back_Click(object sender, EventArgs e) {
             this.Close();
+        }
+
+
+        private void createUser(string username, string password) {
+            string passDatabase = "passDatabase.txt";
+            bool userExists = false;
+            if (File.Exists(passDatabase)) {
+                string[] lines = File.ReadAllLines(passDatabase);
+                foreach (string line in lines) {
+                    string[] values = line.Split(',');
+                    if (values[0] == username) {
+                        userExists = true;
+                        break;
+                    }
+                }
+            }
+            else {
+                using (FileStream create = File.Create(passDatabase)) ;
+                MessageBox.Show("Password database not found\nPerforming First run Operation", "Error", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+            }
+            if (!userExists) { 
+            using (StreamWriter sw1 = new StreamWriter(passDatabase,true)) {
+                sw1.WriteLine($"{username},{password}");
+                MessageBox.Show("Account has been created", "Complete", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+            }
+        }
+            else {
+                MessageBox.Show("Username Taken\nTry Login or use Another username", "Error", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+            }
         }
     }
 }
--- 3112FinalProject/landPage.cs
+++ 3112FinalProject/landPage.cs
@@ -2,7 +2,6 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
-using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -11,15 +10,11 @@ using System.Windows.Forms;
 
 namespace _3112FinalProject {
     public partial class landPage : Form {
-        SqlCommand cmd;
-        SqlConnection cn;
-        SqlDataReader dr;
         public landPage() {
             InitializeComponent();
         }
         private void Form3_Load(object sender, EventArgs e) {
-            cn = new SqlConnection(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=|DataDirectory|\Database1.mdf;Integrated Security=True");
-            cn.Open();
+
         }
 
         private void label1_Click(object sender, EventArgs e) {
